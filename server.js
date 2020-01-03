c78790cbb4e461c257d391f93e94eba72f36e18e
GustavoCedeno <gustavo.a.cedeno@gmail.com>
2020-01-03 16:56:33 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const fetch = require('node-fetch')
+// Start up an instance of app
+const app = express()
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors())
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+
+const port = 8000;
+// Spin up the server
+const server = app.listen(port, listening);
+ function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
+
+// ROUTES!
+app.get('/all',(req,res)=>{
+    res.send(projectData);
+});
+
+// POST route adds data to ProjectData
+app.post('/add', function (request, response) {
+    newEntry = {
+        temperature: request.body.temperature,
+        date: request.body.date,
+        userResponse: request.body.userResponse
+    };
+
+    projectData.unshift(newEntry);
+});
